8ec7b4b478bcd049d3fbcb655db6149b908c13f1
Paul Rozhkin <rozhkin@protei.ru>
2019-12-06 14:35:34 +0300

    Add project files.

---
+++ Curse_CSharpProgramming_FromNoviceToSpecialist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curse_CSharpProgramming_FromNoviceToSpecialist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
